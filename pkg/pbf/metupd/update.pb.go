09d96e58c59ca29d3c805ef4ab10fc46f87b21b0
xh3b4sd <xh3b4sd@users.noreply.github.com>
2020-11-13 16:58:14 +0000

    update generated code

--- pkg/pbf/metupd/update.pb.go
+++ pkg/pbf/metupd/update.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // 	protoc-gen-go v1.25.0
 // 	protoc        v3.13.0
-// source: pbf/metric/update.proto
+// source: pbf/metupd/update.proto
 
-package metric
+package metupd
 
 import (
 	proto "github.com/golang/protobuf/proto"
@@ -34,7 +34,7 @@ type UpdateI struct {
 func (x *UpdateI) Reset() {
 	*x = UpdateI{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_pbf_metric_update_proto_msgTypes[0]
+		mi := &file_pbf_metupd_update_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -47,7 +47,7 @@ func (x *UpdateI) String() string {
 func (*UpdateI) ProtoMessage() {}
 
 func (x *UpdateI) ProtoReflect() protoreflect.Message {
-	mi := &file_pbf_metric_update_proto_msgTypes[0]
+	mi := &file_pbf_metupd_update_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -60,7 +60,7 @@ func (x *UpdateI) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use UpdateI.ProtoReflect.Descriptor instead.
 func (*UpdateI) Descriptor() ([]byte, []int) {
-	return file_pbf_metric_update_proto_rawDescGZIP(), []int{0}
+	return file_pbf_metupd_update_proto_rawDescGZIP(), []int{0}
 }
 
 type UpdateO struct {
@@ -72,7 +72,7 @@ type UpdateO struct {
 func (x *UpdateO) Reset() {
 	*x = UpdateO{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_pbf_metric_update_proto_msgTypes[1]
+		mi := &file_pbf_metupd_update_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -85,7 +85,7 @@ func (x *UpdateO) String() string {
 func (*UpdateO) ProtoMessage() {}
 
 func (x *UpdateO) ProtoReflect() protoreflect.Message {
-	mi := &file_pbf_metric_update_proto_msgTypes[1]
+	mi := &file_pbf_metupd_update_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -98,37 +98,37 @@ func (x *UpdateO) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use UpdateO.ProtoReflect.Descriptor instead.
 func (*UpdateO) Descriptor() ([]byte, []int) {
-	return file_pbf_metric_update_proto_rawDescGZIP(), []int{1}
+	return file_pbf_metupd_update_proto_rawDescGZIP(), []int{1}
 }
 
-var File_pbf_metric_update_proto protoreflect.FileDescriptor
+var File_pbf_metupd_update_proto protoreflect.FileDescriptor
 
-var file_pbf_metric_update_proto_rawDesc = []byte{
-	0x0a, 0x17, 0x70, 0x62, 0x66, 0x2f, 0x6d, 0x65, 0x74, 0x72, 0x69, 0x63, 0x2f, 0x75, 0x70, 0x64,
-	0x61, 0x74, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x06, 0x6d, 0x65, 0x74, 0x72, 0x69,
-	0x63, 0x22, 0x09, 0x0a, 0x07, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x49, 0x22, 0x09, 0x0a, 0x07,
+var file_pbf_metupd_update_proto_rawDesc = []byte{
+	0x0a, 0x17, 0x70, 0x62, 0x66, 0x2f, 0x6d, 0x65, 0x74, 0x75, 0x70, 0x64, 0x2f, 0x75, 0x70, 0x64,
+	0x61, 0x74, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x06, 0x6d, 0x65, 0x74, 0x75, 0x70,
+	0x64, 0x22, 0x09, 0x0a, 0x07, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x49, 0x22, 0x09, 0x0a, 0x07,
 	0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x4f, 0x42, 0x0a, 0x5a, 0x08, 0x2e, 0x3b, 0x6d, 0x65, 0x74,
-	0x72, 0x69, 0x63, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x75, 0x70, 0x64, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_pbf_metric_update_proto_rawDescOnce sync.Once
-	file_pbf_metric_update_proto_rawDescData = file_pbf_metric_update_proto_rawDesc
+	file_pbf_metupd_update_proto_rawDescOnce sync.Once
+	file_pbf_metupd_update_proto_rawDescData = file_pbf_metupd_update_proto_rawDesc
 )
 
-func file_pbf_metric_update_proto_rawDescGZIP() []byte {
-	file_pbf_metric_update_proto_rawDescOnce.Do(func() {
-		file_pbf_metric_update_proto_rawDescData = protoimpl.X.CompressGZIP(file_pbf_metric_update_proto_rawDescData)
+func file_pbf_metupd_update_proto_rawDescGZIP() []byte {
+	file_pbf_metupd_update_proto_rawDescOnce.Do(func() {
+		file_pbf_metupd_update_proto_rawDescData = protoimpl.X.CompressGZIP(file_pbf_metupd_update_proto_rawDescData)
 	})
-	return file_pbf_metric_update_proto_rawDescData
+	return file_pbf_metupd_update_proto_rawDescData
 }
 
-var file_pbf_metric_update_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_pbf_metric_update_proto_goTypes = []interface{}{
-	(*UpdateI)(nil), // 0: metric.UpdateI
-	(*UpdateO)(nil), // 1: metric.UpdateO
+var file_pbf_metupd_update_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_pbf_metupd_update_proto_goTypes = []interface{}{
+	(*UpdateI)(nil), // 0: metupd.UpdateI
+	(*UpdateO)(nil), // 1: metupd.UpdateO
 }
-var file_pbf_metric_update_proto_depIdxs = []int32{
+var file_pbf_metupd_update_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -136,13 +136,13 @@ var file_pbf_metric_update_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_pbf_metric_update_proto_init() }
-func file_pbf_metric_update_proto_init() {
-	if File_pbf_metric_update_proto != nil {
+func init() { file_pbf_metupd_update_proto_init() }
+func file_pbf_metupd_update_proto_init() {
+	if File_pbf_metupd_update_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_pbf_metric_update_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_pbf_metupd_update_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*UpdateI); i {
 			case 0:
 				return &v.state
@@ -154,7 +154,7 @@ func file_pbf_metric_update_proto_init() {
 				return nil
 			}
 		}
-		file_pbf_metric_update_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_pbf_metupd_update_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*UpdateO); i {
 			case 0:
 				return &v.state
@@ -171,18 +171,18 @@ func file_pbf_metric_update_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_pbf_metric_update_proto_rawDesc,
+			RawDescriptor: file_pbf_metupd_update_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_pbf_metric_update_proto_goTypes,
-		DependencyIndexes: file_pbf_metric_update_proto_depIdxs,
-		MessageInfos:      file_pbf_metric_update_proto_msgTypes,
+		GoTypes:           file_pbf_metupd_update_proto_goTypes,
+		DependencyIndexes: file_pbf_metupd_update_proto_depIdxs,
+		MessageInfos:      file_pbf_metupd_update_proto_msgTypes,
 	}.Build()
-	File_pbf_metric_update_proto = out.File
-	file_pbf_metric_update_proto_rawDesc = nil
-	file_pbf_metric_update_proto_goTypes = nil
-	file_pbf_metric_update_proto_depIdxs = nil
+	File_pbf_metupd_update_proto = out.File
+	file_pbf_metupd_update_proto_rawDesc = nil
+	file_pbf_metupd_update_proto_goTypes = nil
+	file_pbf_metupd_update_proto_depIdxs = nil
 }
