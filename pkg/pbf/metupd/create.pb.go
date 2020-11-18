9c2fdb312f0e7280b76cb8714aad6101c71c8909
xh3b4sd <xh3b4sd@users.noreply.github.com>
2020-11-18 17:06:27 +0000

    update generated code

--- pkg/pbf/metupd/create.pb.go
+++ pkg/pbf/metupd/create.pb.go
@@ -338,7 +338,8 @@ type CreateO struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Metadata map[string]string `protobuf:"bytes,1,rep,name=metadata,proto3" json:"metadata,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+	Api *CreateO_API `protobuf:"bytes,1,opt,name=api,proto3" json:"api,omitempty"`
+	Obj *CreateO_Obj `protobuf:"bytes,2,opt,name=obj,proto3" json:"obj,omitempty"`
 }
 
 func (x *CreateO) Reset() {
@@ -373,7 +374,99 @@ func (*CreateO) Descriptor() ([]byte, []int) {
 	return file_pbf_metupd_create_proto_rawDescGZIP(), []int{5}
 }
 
-func (x *CreateO) GetMetadata() map[string]string {
+func (x *CreateO) GetApi() *CreateO_API {
+	if x != nil {
+		return x.Api
+	}
+	return nil
+}
+
+func (x *CreateO) GetObj() *CreateO_Obj {
+	if x != nil {
+		return x.Obj
+	}
+	return nil
+}
+
+type CreateO_API struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *CreateO_API) Reset() {
+	*x = CreateO_API{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_pbf_metupd_create_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *CreateO_API) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*CreateO_API) ProtoMessage() {}
+
+func (x *CreateO_API) ProtoReflect() protoreflect.Message {
+	mi := &file_pbf_metupd_create_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use CreateO_API.ProtoReflect.Descriptor instead.
+func (*CreateO_API) Descriptor() ([]byte, []int) {
+	return file_pbf_metupd_create_proto_rawDescGZIP(), []int{6}
+}
+
+type CreateO_Obj struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Metadata map[string]string `protobuf:"bytes,1,rep,name=metadata,proto3" json:"metadata,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+}
+
+func (x *CreateO_Obj) Reset() {
+	*x = CreateO_Obj{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_pbf_metupd_create_proto_msgTypes[7]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *CreateO_Obj) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*CreateO_Obj) ProtoMessage() {}
+
+func (x *CreateO_Obj) ProtoReflect() protoreflect.Message {
+	mi := &file_pbf_metupd_create_proto_msgTypes[7]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use CreateO_Obj.ProtoReflect.Descriptor instead.
+func (*CreateO_Obj) Descriptor() ([]byte, []int) {
+	return file_pbf_metupd_create_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *CreateO_Obj) GetMetadata() map[string]string {
 	if x != nil {
 		return x.Metadata
 	}
@@ -414,10 +507,17 @@ var file_pbf_metupd_create_proto_rawDesc = []byte{
 	0x6a, 0x5f, 0x50, 0x72, 0x6f, 0x70, 0x65, 0x72, 0x74, 0x79, 0x5f, 0x44, 0x61, 0x74, 0x61, 0x12,
 	0x14, 0x0a, 0x05, 0x73, 0x70, 0x61, 0x63, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05,
 	0x73, 0x70, 0x61, 0x63, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02,
-	0x20, 0x03, 0x28, 0x03, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x81, 0x01, 0x0a, 0x07,
-	0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x4f, 0x12, 0x39, 0x0a, 0x08, 0x6d, 0x65, 0x74, 0x61, 0x64,
-	0x61, 0x74, 0x61, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x1d, 0x2e, 0x6d, 0x65, 0x74, 0x75,
-	0x70, 0x64, 0x2e, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x4f, 0x2e, 0x4d, 0x65, 0x74, 0x61, 0x64,
+	0x20, 0x03, 0x28, 0x03, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x22, 0x57, 0x0a, 0x07, 0x43,
+	0x72, 0x65, 0x61, 0x74, 0x65, 0x4f, 0x12, 0x25, 0x0a, 0x03, 0x61, 0x70, 0x69, 0x18, 0x01, 0x20,
+	0x01, 0x28, 0x0b, 0x32, 0x13, 0x2e, 0x6d, 0x65, 0x74, 0x75, 0x70, 0x64, 0x2e, 0x43, 0x72, 0x65,
+	0x61, 0x74, 0x65, 0x4f, 0x5f, 0x41, 0x50, 0x49, 0x52, 0x03, 0x61, 0x70, 0x69, 0x12, 0x25, 0x0a,
+	0x03, 0x6f, 0x62, 0x6a, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x13, 0x2e, 0x6d, 0x65, 0x74,
+	0x75, 0x70, 0x64, 0x2e, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x4f, 0x5f, 0x4f, 0x62, 0x6a, 0x52,
+	0x03, 0x6f, 0x62, 0x6a, 0x22, 0x0d, 0x0a, 0x0b, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x4f, 0x5f,
+	0x41, 0x50, 0x49, 0x22, 0x89, 0x01, 0x0a, 0x0b, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x4f, 0x5f,
+	0x4f, 0x62, 0x6a, 0x12, 0x3d, 0x0a, 0x08, 0x6d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x18,
+	0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x21, 0x2e, 0x6d, 0x65, 0x74, 0x75, 0x70, 0x64, 0x2e, 0x43,
+	0x72, 0x65, 0x61, 0x74, 0x65, 0x4f, 0x5f, 0x4f, 0x62, 0x6a, 0x2e, 0x4d, 0x65, 0x74, 0x61, 0x64,
 	0x61, 0x74, 0x61, 0x45, 0x6e, 0x74, 0x72, 0x79, 0x52, 0x08, 0x6d, 0x65, 0x74, 0x61, 0x64, 0x61,
 	0x74, 0x61, 0x1a, 0x3b, 0x0a, 0x0d, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x45, 0x6e,
 	0x74, 0x72, 0x79, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
@@ -439,7 +539,7 @@ func file_pbf_metupd_create_proto_rawDescGZIP() []byte {
 	return file_pbf_metupd_create_proto_rawDescData
 }
 
-var file_pbf_metupd_create_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
+var file_pbf_metupd_create_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
 var file_pbf_metupd_create_proto_goTypes = []interface{}{
 	(*CreateI)(nil),                   // 0: metupd.CreateI
 	(*CreateI_API)(nil),               // 1: metupd.CreateI_API
@@ -447,21 +547,25 @@ var file_pbf_metupd_create_proto_goTypes = []interface{}{
 	(*CreateI_Obj_Property)(nil),      // 3: metupd.CreateI_Obj_Property
 	(*CreateI_Obj_Property_Data)(nil), // 4: metupd.CreateI_Obj_Property_Data
 	(*CreateO)(nil),                   // 5: metupd.CreateO
-	nil,                               // 6: metupd.CreateI_Obj.MetadataEntry
-	nil,                               // 7: metupd.CreateO.MetadataEntry
+	(*CreateO_API)(nil),               // 6: metupd.CreateO_API
+	(*CreateO_Obj)(nil),               // 7: metupd.CreateO_Obj
+	nil,                               // 8: metupd.CreateI_Obj.MetadataEntry
+	nil,                               // 9: metupd.CreateO_Obj.MetadataEntry
 }
 var file_pbf_metupd_create_proto_depIdxs = []int32{
 	1, // 0: metupd.CreateI.api:type_name -> metupd.CreateI_API
 	2, // 1: metupd.CreateI.obj:type_name -> metupd.CreateI_Obj
-	6, // 2: metupd.CreateI_Obj.metadata:type_name -> metupd.CreateI_Obj.MetadataEntry
+	8, // 2: metupd.CreateI_Obj.metadata:type_name -> metupd.CreateI_Obj.MetadataEntry
 	3, // 3: metupd.CreateI_Obj.property:type_name -> metupd.CreateI_Obj_Property
 	4, // 4: metupd.CreateI_Obj_Property.data:type_name -> metupd.CreateI_Obj_Property_Data
-	7, // 5: metupd.CreateO.metadata:type_name -> metupd.CreateO.MetadataEntry
-	6, // [6:6] is the sub-list for method output_type
-	6, // [6:6] is the sub-list for method input_type
-	6, // [6:6] is the sub-list for extension type_name
-	6, // [6:6] is the sub-list for extension extendee
-	0, // [0:6] is the sub-list for field type_name
+	6, // 5: metupd.CreateO.api:type_name -> metupd.CreateO_API
+	7, // 6: metupd.CreateO.obj:type_name -> metupd.CreateO_Obj
+	9, // 7: metupd.CreateO_Obj.metadata:type_name -> metupd.CreateO_Obj.MetadataEntry
+	8, // [8:8] is the sub-list for method output_type
+	8, // [8:8] is the sub-list for method input_type
+	8, // [8:8] is the sub-list for extension type_name
+	8, // [8:8] is the sub-list for extension extendee
+	0, // [0:8] is the sub-list for field type_name
 }
 
 func init() { file_pbf_metupd_create_proto_init() }
@@ -542,6 +646,30 @@ func file_pbf_metupd_create_proto_init() {
 				return nil
 			}
 		}
+		file_pbf_metupd_create_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*CreateO_API); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_pbf_metupd_create_proto_msgTypes[7].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*CreateO_Obj); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -549,7 +677,7 @@ func file_pbf_metupd_create_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_pbf_metupd_create_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   8,
+			NumMessages:   10,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
