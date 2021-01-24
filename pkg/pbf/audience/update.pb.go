84961901463140aa18cb65253b66ca9d4217963e
xh3b4sd <xh3b4sd@users.noreply.github.com>
2021-01-24 14:10:00 +0000

    update generated code

--- pkg/pbf/audience/update.pb.go
+++ pkg/pbf/audience/update.pb.go
@@ -30,6 +30,7 @@ const _ = proto.ProtoPackageIsVersion4
 //     {
 //         "obj": {
 //             "metadata": {
+//                 "audience.venturemark.co/id": "<id>",
 //                 "organization.venturemark.co/id": "<id>",
 //                 "user.venturemark.co/id": "<id>"
 //             },
